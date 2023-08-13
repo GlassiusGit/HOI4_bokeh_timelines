9758bf227517a726a434fe93c19413a070321b01
Glassius <glassius_89@o2.pl>
2023-08-13 23:59:09 +0200

    Circle text rendered for real events. Also with scalling

--- main.py
+++ main.py
@@ -72,21 +72,6 @@ glyph_block_text = Text(
 block_renderer = p.add_glyph(vanilla_source, glyph_block)
 glyph_text_renderer = p.add_glyph(vanilla_source, glyph_block_text)
 
-
-callback_scaling_text = CustomJS(
-    args=dict(
-        glyph_block_text=glyph_block_text,
-        original_size=original_size,
-        p=p,
-        font_size=font_size
-        ),
-    code="""
-const new_size = Math.floor(original_size / (p.x_range.end - p.x_range.start)  * font_size);
-glyph_block_text.text_font_size = new_size + "px";
-""")
-p.x_range.js_on_change('end', callback_scaling_text)
-p.x_range.js_on_change('start', callback_scaling_text)
-
 block_hover = HoverTool(renderers=[block_renderer], tooltips=[("", "@focus"),
                                                               ("", "@date_range"),
                                                               ("", "@hover_text")])
@@ -110,24 +95,57 @@ for i, event in enumerate(events):
     circle_colors.append(color)
     connection_colors.append(color)
     
-    
+circle_radius = td(days=3)
 circle_source = ColumnDataSource({
     "x": dates,
-    "y": [-1] * len(events),
-    "color": circle_colors
+    "y_circles": [-1] * len(events),
+    "y_text": [-1.1]*len(events),
+    "color": circle_colors,
+    "events": events,
+    "text_font": ["courier"]*len(events),
+    "angles": [-3.14*1/10] * len(events),
+    "text_baselines": ["top"] * len(events)
     })
 
 glyph_circle = Circle(
     x="x",
-    y="y",
-    radius=td(days=3),
+    y="y_circles",
+    radius=circle_radius,
     radius_units="data",
     radius_dimension="x",
-    fill_color="color"
+    fill_color="color",
     )
 
+glyph_circle_text = Text(
+    x="x",
+    y="y_text",
+    text="events",
+    text_font_size = f"{font_size}px",
+    text_font = "text_font",
+    angle="angles",
+    text_baseline="text_baselines"
+    )
+
+
 glyph_circle_renderer = p.add_glyph(circle_source, glyph_circle)
+glyph_circle_text_rendered = p.add_glyph(circle_source, glyph_circle_text)
 
 p.toolbar.active_scroll = "auto"
+
+callback_scaling_block_text = CustomJS(
+    args=dict(
+        glyph_block_text=glyph_block_text,
+        glyph_circle_text=glyph_circle_text,
+        original_size=original_size,
+        p=p,
+        font_size=font_size
+        ),
+    code="""
+const new_size = Math.floor(original_size / (p.x_range.end - p.x_range.start)  * font_size);
+glyph_block_text.text_font_size = new_size + "px";
+glyph_circle_text.text_font_size = new_size + "px";
+""")
+p.x_range.js_on_change('end', callback_scaling_block_text)
+p.x_range.js_on_change('start', callback_scaling_block_text)
 
 show(p)
